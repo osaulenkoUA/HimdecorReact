215808916bae85723a08d384cddde283a6b6417e
Osaulenko Oleksandr <oom2212@gmail.com>
2020-12-09 18:39:47 +0200

    correct contact

---
+++ build/precache-manifest.612b96b473a7f430dc0bbbd6dff05ccd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc29e4130f6bb0da4a50924bac3f238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a9fee82a2dcafcd8fc",
+    "url": "/static/css/3.f89735ef.chunk.css"
+  },
+  {
+    "revision": "1160708a1d5d05214f2c",
+    "url": "/static/css/main.189da11d.chunk.css"
+  },
+  {
+    "revision": "0f70ce636d6cd4eeecfc",
+    "url": "/static/js/2.21ae8be9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.21ae8be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a9fee82a2dcafcd8fc",
+    "url": "/static/js/3.cf46b644.chunk.js"
+  },
+  {
+    "revision": "b9eb2fc838ef0a20b414",
+    "url": "/static/js/4.262825c1.chunk.js"
+  },
+  {
+    "revision": "6a00ee6f0f4110e338c8",
+    "url": "/static/js/5.70e42d83.chunk.js"
+  },
+  {
+    "revision": "c076129d71c898a9a1fc",
+    "url": "/static/js/6.4932753a.chunk.js"
+  },
+  {
+    "revision": "24a52c1cceaf711ebdc1",
+    "url": "/static/js/7.93ec3da1.chunk.js"
+  },
+  {
+    "revision": "ab6de6be6c887f40ec94",
+    "url": "/static/js/8.871f2163.chunk.js"
+  },
+  {
+    "revision": "1160708a1d5d05214f2c",
+    "url": "/static/js/main.9b180511.chunk.js"
+  },
+  {
+    "revision": "268ac7639ca31b68fd34",
+    "url": "/static/js/runtime-main.8d220c4e.js"
+  },
+  {
+    "revision": "535d71dd8a4bdb9858a7bb232ff8c368",
+    "url": "/static/media/ill_contact.535d71dd.png"
+  },
+  {
+    "revision": "77dfdc20cf6cdb6ae3ccd61af3a8a9fd",
+    "url": "/static/media/map.77dfdc20.jpg"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  }
+]);
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,38 +4,40 @@ import pic from './ill_contact.png';
 
 import s from './Contacts.module.css';
 function Header() {
-	return (
-		<section>
-			<div className={s.contact}>
-				<div className={s.phones}>
-					<a href="tel:+380674310968" className={s.phones__mobile}>
-						+380(67)4310968
-					</a>
-					<p className={s.phones__works}>0432-55-66-18</p>
-				</div>
+  return (
+    <section>
+      <div className={s.contact}>
+        <div className={s.phones}>
+          <a href="tel:+380674310968" className={s.phones__mobile}>
+            +380(67)4310968
+          </a>
+          <p className={s.phones__works}>0432-55-66-18</p>
+        </div>
 
-				<div  className={s.emails}>
-					<p className={s.emails__info}>info@himdecor.ua</p>
-					<p className={s.emails__employer} >mycola.osaulenko@himdecor.ua (директор)</p>
-				</div>
+        <div className={s.emails}>
+          <span className={s.emails__info}>info@himdecor.ua</span>
+          <span className={s.emails__employer}>
+            mycola.osaulenko@himdecor.ua (директор)
+          </span>
+        </div>
 
-				<div  className={s.address}>
-					<p  className={s.address_text} >м.Вінниця, вул. Максима Шимка, 42</p>
-				</div>
-				<div  className={s.web} >
-					<p  className={s.web_text} >www.zakraska.com.ua</p>
-					<p  className={s.web_text} >www.himrezerv.com.ua</p>
-				</div>
+        <div className={s.address}>
+          <p className={s.address_text}>м.Вінниця, вул. Максима Шимка, 42</p>
+        </div>
+        <div className={s.web}>
+          <span className={s.web_text}>www.zakraska.com.ua</span>
+          <span className={s.web_text}>www.himrezerv.com.ua</span>
+        </div>
 
-				<picture  className={s.contact__picture} >
-					<img src={pic} alt="" />
-				</picture>
-			</div>
-			<div  className={s.contactMap} >
-				<img src={map} alt="" />
-			</div>
-		</section>
-	);
+        <picture className={s.contact__picture}>
+          <img src={pic} alt="" />
+        </picture>
+      </div>
+      <div className={s.contactMap}>
+        <img src={map} alt="" />
+      </div>
+    </section>
+  );
 }
 
 export default Header;
